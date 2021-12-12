47e9322aa4a359e047f2aabb43fe492798615f9a
Hunter Nelson <huntknelson@gmail.com>
2021-12-12 01:15:30 -0600

    done with amazing art

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const keys = {
     number: "0123456789",
     symbol: "!@#$%^&*()_=?.,<>/{}[]"
 }
-// checkbox function array
+// getKey array defined
 const getKey = [
     function upperCase() {
         return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
@@ -17,13 +17,39 @@ const getKey = [
         return keys.number[Math.floor(Math.random() * keys.number.length)];
     },
     function symbol() {
-        return keys.symbol[Math.floor(Math.random() * keys.number.length)];
+        return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
     }
 ];
-
-function createPassword () {
-    console.log("hurray!");
-};
-function copyPassword () {
-    console.log("BOOOOO")
-}
+// Create Password Function
+function createPassword() {
+    const upper = document.getElementById("upperCase").checked;
+    const lower = document.getElementById("lowerCase").checked;
+    const number = document.getElementById("number").checked;
+    const symbol = document.getElementById("symbol").checked;
+    if (upper + lower + number + symbol === 0) {
+      alert("Please check atleast one box!");
+      return;
+    }
+    const passwordBox = document.getElementById("passwordBox");
+    const length = document.getElementById("length");
+    let password = "";
+    while (length.value > password.length) {
+      let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
+      let isChecked = document.getElementById(keyToAdd.name).checked;
+      if (isChecked) {
+        password += keyToAdd();
+      }
+    }
+    passwordBox.innerHTML = password;
+  }
+  function copyPassword() {
+    const textarea = document.createElement("textarea");
+    const password = document.getElementById("passwordBox").innerText;
+    if (!password) { return; }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard");
+  }
