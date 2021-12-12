63f427d2f76e531933d66603ebb7897be020142c
Hunter Nelson <huntknelson@gmail.com>
2021-12-12 00:18:25 -0600

    added array of functions to js, might work

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@ const keys = {
     number: "0123456789",
     symbol: "!@#$%^&*()_=?.,<>/{}[]"
 }
+// checkbox function array
+const getKey = [
+    function upperCase() {
+        return keys.upperCase[(Math.random() * keys.upperCase.length)];
+    },
+    function lowerCase() {
+        return keys.lowerCase[(Math.random() * keys.lowerCase.length)]
+    },
+    function number() {
+        return keys.number[(Math.random() * keys.number.length)];
+    },
+    function symbol() {
+        return keys.symbol[(Math.random() * keys.number.length)];
+    }
+];
